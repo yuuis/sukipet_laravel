4e0ea8487d251d6eb0a17ca42892295be98768a4
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-26 09:47:23 +0900

    fix wrong parameter

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ Route::resource("users", "UserController", ["only" => ["create", "store"]]);
 Route::resource("questions", "QuestionController", ["only" => ["create", "store", "show"]]);
 
 # sessions
-Route::get("/login", ["as" => "login", "uses" =>  "SessionController@create"]);
+Route::get("/login", ["as" => "login", "uses" => "SessionController@create"]);
 Route::post("/login", "SessionController@store");
 Route::get("/logout", ["as" => "logout", "uses" => "SessionController@destroy"]);
 
 # answers
-Route::get("/questions/:question_id/messages/create", ["as" => "answers.create", "uses" => "AnswerController@create"]);
-Route::post("/questions/:question_id/messages", ["as" => "answers.store", "uses" => "AnswerController@store"]);
+Route::get("/questions/{question_id}/messages/create", ["as" => "answers.create", "uses" => "AnswerController@create"]);
+Route::post("/questions/{question_id}/messages", ["as" => "answers.store", "uses" => "AnswerController@store"]);
 
 # messages
-Route::get("/questions/:question_id/answer/:answer_id/messages/create", ["as" => "messages.create", "uses" => "MessageController@create"]);
-Route::post("/questions/:question_id/answer/:answer_id/messages", ["as" => "messages.store", "uses" => "MessageController@store"]);
+Route::get("/questions/{question_id}/answer/{answer_id}/messages/create", ["as" => "messages.create", "uses" => "MessageController@create"]);
+Route::post("/questions/{question_id}/answer/{answer_id}/messages", ["as" => "messages.store", "uses" => "MessageController@store"]);
